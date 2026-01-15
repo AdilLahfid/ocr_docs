1592342e4e411f261230effb8bd88d516667a3c0
badr eddine el hassouni <abdo.sbeta.15@gmail.com>
2026-01-15 20:25:10 +0100

    Add new Python script: scripts/script_20260115_202508.py

---
+++ scripts/script_20260115_202508.py
@@ -0,0 +1,8 @@
+# Auto-generated script
+# Created: 2026-01-15T20:25:08.366418
+
+def reverse_string(s):
+    return s[::-1]
+
+if __name__ == "__main__":
+    print("Script executed successfully!")
