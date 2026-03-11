34bc594a626af703fb3e05b08effebfd23501e7e
badr eddine el hassouni <abdo.sbeta.15@gmail.com>
2026-03-11 13:18:08 +0000

    Add Python script: scripts/script_20260311_131805.py

---
+++ scripts/script_20260311_131805.py
@@ -0,0 +1,9 @@
+# Auto-generated script
+# Created: 2026-03-11T13:18:05.726954
+# Account: Account 2
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+if __name__ == "__main__":
+    print("Script executed successfully!")
